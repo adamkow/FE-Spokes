8f0a8c0f5b7c91849eed7728dba9371087ebeee5
Ewelina <ewelinazawol@gmail.com>
2024-04-02 11:01:53 +0100

    user list layout

--- app/(tabs)/UserList.jsx
+++ app/(tabs)/UserList.jsx
@@ -1,24 +1,19 @@
-
-import { getAllUsers } from "@/api";
-import { useEffect, useState } from "react";
-import { useAuth } from "@/contexts/authContext";
-import {
-  FlatList,
-  Text,
-  View,
-  Modal,
-  StyleSheet,
-  Pressable,
-  Button,
-} from 'react-native'
+import { getAllUsers } from '@/api'
+import { useEffect, useState } from 'react'
+import { useAuth } from '@/contexts/authContext'
+import { FlatList, Text, Pressable, ScrollView, View } from 'react-native'
 import UserCard from '../../components/UserCard'
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context'
 
 export default function Users() {
-  const [userList, setUserList] = useState([]);
-  const {logout} = useAuth()
+  const [userList, setUserList] = useState([])
+  const { logout } = useAuth()
 
   const handleLogout = async () => {
-    await logout();
+    await logout()
   }
 
   useEffect(() => {
@@ -29,16 +24,20 @@ export default function Users() {
 
   return (
     <>
-      <View className="flex-1 justify-center items-center">
-      <Pressable onPress={handleLogout} className="pt-5"><Text>Log Out</Text></Pressable>
-        <FlatList
-          data={userList}
-          renderItem={({ item }) => (
-            <UserCard user={item} setUserList={setUserList} />
-          )}
-          keyExtractor={(item) => item.user_id}
-        />
-      </View>
+      <SafeAreaProvider>
+        <View showsVerticalScrollIndicator className="flex-1">
+          <Pressable onPress={handleLogout} className="pt-5">
+            <Text>Log Out</Text>
+          </Pressable>
+          <FlatList
+            data={userList}
+            renderItem={({ item }) => (
+              <UserCard user={item} setUserList={setUserList} />
+            )}
+            keyExtractor={(item) => item.user_id}
+          />
+        </View>
+      </SafeAreaProvider>
     </>
   )
 }
