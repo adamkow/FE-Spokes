994d51ba807b845ae51a7c22bff4204a2eefc2ae
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-04-02 16:30:52 +0100

    Edit profile now uses current user

--- app/(tabs)/UserList.jsx
+++ app/(tabs)/UserList.jsx
@@ -1,18 +1,12 @@
-
 import { getAllUsers } from '@/api'
 import { useEffect, useState } from 'react'
 import { useAuth } from '@/contexts/authContext'
-import { FlatList, Text, Pressable, View } from 'react-native'
+import { FlatList, View } from 'react-native'
 import UserCard from '../../components/UserCard'
 
 export default function Users() {
   const [userList, setUserList] = useState([])
-  const { logout } = useAuth()
-
-  const handleLogout = async () => {
-    await logout()
-  }
-
+  const { user } = useAuth()
 
   useEffect(() => {
     getAllUsers().then((users) => {
@@ -26,7 +20,9 @@ export default function Users() {
         <FlatList
           data={userList}
           renderItem={({ item }) => (
-            <UserCard user={item} setUserList={setUserList} />
+            item.city === user.city ? 
+              <UserCard user={item} setUserList={setUserList} /> : 
+              <></>
           )}
           keyExtractor={(item) => item.user_id}
         />
